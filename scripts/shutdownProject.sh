8d0a57c5bde88bce556cfff876e2ee281a895a2f
Anikeiev Ihor <anikeev.ihor@gmail.com>
2023-02-08 22:07:17 +0200

    update shutdownProject.sh

--- scripts/shutdownProject.sh
+++ scripts/shutdownProject.sh
@@ -7,14 +7,14 @@ CREDENTIAL_SA_JSON=terraform-sa.json
 BUCKET_NAME=tf-bookshelf-ianikeiev
 BUCKET_ROLES=("roles/storage.legacyBucketOwner" "roles/storage.legacyObjectOwner")
 
-terraform destroy -auto-approve
-cd ..
 #[START Delete Source Repository]
 # shellcheck disable=SC2207
 REPOS=($(gcloud source repos list --filter="Name:'$PROJECT_ID'" --format="value(name)"))
 for i in ${REPOS[*]}; do
     gcloud source repos list --filter="name:'$PROJECT_ID'" --format="value(name)" | grep -q "$i"
+    # shellcheck disable=SC2181
     if [ $? -eq 0 ]; then
+      # shellcheck disable=SC2006
       EXECUTE=`gcloud source repos delete "$i" --quiet`
       echo "Deleted Repo Name: $i$EXECUTE. Done"
     fi
@@ -23,8 +23,6 @@ for i in ${REPOS[*]}; do
 
 for role in ${BUCKET_ROLES[*]}; do
   gcloud storage buckets remove-iam-policy-binding gs://$BUCKET_NAME --member=serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com --role="$role"
-  #gcloud storage buckets add-iam-policy-binding gs://$BUCKET_NAME --member=serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com --role="$role"
-  #gcloud storage buckets add-iam-policy-binding gs://$BUCKET_NAME --member=serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com --role=roles/storage.legacyObjectOwner
 done
 
 #[START Delete Storage Bucket]
@@ -35,22 +33,22 @@ gcloud storage buckets delete gs://$BUCKET_NAME
 
 
 #[START REMOVE SERVICE ACCOUNT AND IAM POLICY BINDING]
-#gcloud iam service-accounts delete $SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com
 if gcloud iam service-accounts list --filter="displayName:$SERVICE_ACCOUNT" --format="table(displayName)" | grep -q $SERVICE_ACCOUNT; then
   for role in ${SA_ROLES[*]}; do
     gcloud projects remove-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" --role="$role"
   done
-  #gcloud iam service-accounts delete terraform-sa@try-bookshelf-ianikeiev.iam.gserviceaccount.com --quiet
   echo "Delete Service Account $SERVICE_ACCOUNT"
   gcloud iam service-accounts delete $SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com --quiet
 else
   echo "Servie Account Absent"
 fi
 #[END REMOVE IAM POLICY BINDING]
-#gcloud services disable $PROJECT_API
 
 #rm $CREDENTIAL_SA_JSON
-rm $HOME/.config/gcloud/$CREDENTIAL_SA_JSON
-#Unlink Billing from Project
+rm "$HOME"/.config/gcloud/$CREDENTIAL_SA_JSON
+
+#[Unlink Billing from Project]
 #gcloud beta billing projects unlink $PROJECT_ID
 rm -rf Infra/
+rm -rf InstanceConfig/
+rm -rf bookshelfApp/
